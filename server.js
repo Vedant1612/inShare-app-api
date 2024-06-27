1683bd5c7b7058a39aa795c95f43c23eec369efd
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-06-27 15:42:13 +0530

    Add download page with ejs support

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 const connectDB = require('./config/db');
 connectDB();
 
+// Templet engine
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
 // Routes
-app.use('/api/files', require('./routes/files'))
+app.use('/api/files', require('./routes/files'));
+
+app.use('/files', require('./routes/show'));
 
 app.listen(PORT, ()=>{
     console.log(`Listening on port: ${PORT}`);
